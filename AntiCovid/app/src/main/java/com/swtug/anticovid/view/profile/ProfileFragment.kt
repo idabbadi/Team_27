f5eace07e01aa2f6988dfa46d329b239069c2def
Balint Csordas <balint.csordas@student.tugraz.at>
2021-05-05 14:56:18 +0200

    ANTIC-026 Made profile values localy editable [BC, NG]

--- AntiCovid/app/src/main/java/com/swtug/anticovid/view/profile/ProfileFragment.kt
+++ AntiCovid/app/src/main/java/com/swtug/anticovid/view/profile/ProfileFragment.kt
@@ -7,6 +7,7 @@ import android.view.ViewGroup
 import android.widget.Button
 import android.widget.EditText
 import androidx.navigation.fragment.findNavController
+import com.swtug.anticovid.models.User
 import com.swtug.anticovid.R
 import com.swtug.anticovid.repositories.PreferencesRepo
 import com.swtug.anticovid.view.BaseFragment
@@ -14,6 +15,7 @@ import com.swtug.anticovid.view.BaseFragment
 class ProfileFragment : BaseFragment() {
 
     private lateinit var btnlogout: Button
+    private lateinit var btnedit: Button
     private lateinit var editTextName: EditText
     private lateinit var editTextSurname: EditText
     private lateinit var editTextEMail: EditText
@@ -55,11 +57,34 @@ class ProfileFragment : BaseFragment() {
             PreferencesRepo.deleteUser(requireContext())
             PreferencesRepo.deleteVaccination(requireContext())
             findNavController().navigate(R.id.action_profileFragment_to_loginFragment)
+
+        }
+
+        btnedit.setOnClickListener {
+            val old_user = PreferencesRepo.getUser(requireContext())
+            if(old_user!=null)
+            {
+                val user = User(
+                    old_user.id,
+                    editTextName.text.toString(),
+                    editTextSurname.text.toString(),
+                    editTextEMail.text.toString(),
+                    editTextAddress.text.toString(),
+                    editTextSocialSecurityID.text.toString(),
+                    editTextPhoneNumber.text.toString(),
+                    old_user.password
+                )
+                PreferencesRepo.saveUser(requireContext(),user)
+            }
+
+
+
         }
     }
 
     private fun initFields(view: View) {
         btnlogout = view.findViewById(R.id.logoutbutton)
+        btnedit = view.findViewById(R.id.button2)
         editTextName = view.findViewById(R.id.editTextProfileName)
         editTextSurname = view.findViewById(R.id.editTextProfileSurname)
         editTextEMail = view.findViewById(R.id.editTextProfileEmail)
